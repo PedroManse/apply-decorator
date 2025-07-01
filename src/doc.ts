35028a1b9df1bd6d9b39f7406e3c870aedfdb968
Pedro Manse <pedro.manse@dmk3.com.br>
2025-07-01 10:15:21 -0300

    doc/doc: explicitly return FnDoc

--- src/doc.ts
+++ src/doc.ts
@@ -2,7 +2,7 @@ import {Fn} from "./decorators";
 
 type DocFn<I extends unknown[], O> = Fn<I, O> & {doc?: Record<string, string>};
 export function doc({key, value}:{key: string, value: string}) {
-	return function<I extends any[], O>(fn: DocFn<I, O>) {
+	return function<I extends any[], O>(fn: DocFn<I, O>): DocFn<I, O> {
 		fn.doc = fn.doc??{};
 		fn.doc[key] = value;
 		return fn;
